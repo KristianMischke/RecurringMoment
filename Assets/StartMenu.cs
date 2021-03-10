75c69cca87471ad1991398147fca6fc0d28c9610
Wilsonfang1 <wilsonf1@umbc.edu>
2021-03-10 14:19:56 -0500

    Starting scene done-ish
    For some reason could not get the button to click so just made it so if they click the mouse it goes to the first level

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI; 
+public class StartMenu : MonoBehaviour
+{
+	private int nextScene = 0; // the current first level in the build may need to change later if it gets altered 
+	//public Button button;
+
+	
+	void Start()
+	{
+		// nextScene = SceneManager.GetActiveScene().buildIndex - 1; // if we want it to change to another scene depending on 
+		// which one we want we can change it here 
+		//button = GetComponent<Button>(); 
+	}
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))//EventSystem.current.currentSelectedGameObject == button)
+		{
+			Debug.Log("Mouse is pressed down");
+			SceneManager.LoadScene(nextScene); 
+		}
+	}
+	
+}
