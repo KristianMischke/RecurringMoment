f262d6a9a916077ba48d4f933b9c1ea2bb883693
Wilsonfang1 <wilsonf1@umbc.edu>
2021-03-12 13:09:09 -0500

    Finished the button
    I feel really really dumb....

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -7,22 +7,17 @@ using UnityEngine.UI;
 public class StartMenu : MonoBehaviour
 {
 	private int nextScene = 0; // the current first level in the build may need to change later if it gets altered 
-	//public Button button;
-
-	
+	public Button button;
 	void Start()
 	{
-		// nextScene = SceneManager.GetActiveScene().buildIndex - 1; // if we want it to change to another scene depending on 
-		// which one we want we can change it here 
-		//button = GetComponent<Button>(); 
+		button.onClick.AddListener(NextLevel);
+		
 	}
-	void Update()
+	
+	void NextLevel()
 	{
-		if (Input.GetMouseButtonDown(0))//EventSystem.current.currentSelectedGameObject == button)
-		{
-			Debug.Log("Mouse is pressed down");
-			SceneManager.LoadScene(nextScene); 
-		}
+		Debug.Log("You have clicked the button"); 
+		SceneManager.LoadScene(nextScene); 
 	}
 	
 }
