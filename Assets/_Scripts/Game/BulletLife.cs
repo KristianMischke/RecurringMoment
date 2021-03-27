4cfa541969aa9ebedc5f0690abde907fd6f9ff06
AaronG22 <pb21338@umbc.edu>
2021-03-26 22:15:04 -0400

    basic guard stuff

---
+++ Assets/_Scripts/Game/BulletLife.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BulletLife : MonoBehaviour
+{
+
+    public int framesActive = 120;
+    int currentFrame = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentFrame++;
+        if(currentFrame >= framesActive)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("aaronTest");
+        }
+    }
+}
---
+++ Assets/_Scripts/Game/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bullet, player;
+    public float fireRateSeconds = 3f, shotSpeed = 1f, range = 5f;
+    float dist = 0, toFire = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        dist = Mathf.Abs(Vector2.Distance(gameObject.transform.position, player.transform.position));
+        toFire -= Time.deltaTime;
+        if(dist <= range && toFire <= 0)
+        {
+            Blast(player.GetComponent<CapsuleCollider2D>());
+            toFire = fireRateSeconds;
+        }
+        
+    }
+
+
+    void Blast(Collider2D target)
+    {
+
+        GameObject b = Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+        Rigidbody2D r = b.GetComponent<Rigidbody2D>();
+        r.velocity = (target.gameObject.transform.position - gameObject.transform.position).normalized * shotSpeed;
+
+    }
+}
